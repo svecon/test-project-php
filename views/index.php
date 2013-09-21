bc76c0563f43d10a8525e0ec1fbbc910166543ca
Ondřej Švec <ond.svec@gmail.com>
2013-09-21 15:52:55 +0200

    Added filtering by city (select)

--- views/index.php
+++ views/index.php
@@ -1,19 +1,26 @@
 <div class="jumbotron">
-  <div class="container">
-    <h1>PHP Test Application</h1>
-  </div>
+	<div class="container">
+		<h1>PHP Test Application</h1>
+	</div>
 </div>
 
 <h2>User list</h2>
 
-<select id="js-city" class="form-control">
-	<option value="">-- All cities</option>
-	<?php foreach ($cities as $city) { ?>
-		<option value="<?=$city; ?>"><?=$city; ?></option>
-	<?php } ?>
-</select>
+<form method="post" action="create.php" class="form-horizontal" role="form">
+	<label for="email" class="col-lg-offset-8 col-lg-2 control-label">Filter by city:</label>
+	<div class="col-lg-2">
+		<div class="input-group" id="js-filters">
+			<select id="js-city" class="form-control">
+				<option value="">-- All cities</option>
+				<?php foreach ($cities as $city) { ?>
+				<option value="<?=$city; ?>"><?=$city; ?></option>
+				<?php } ?>
+			</select>
+		</div>
+	</div>
+</form>
 
-<table class="table table-striped">
+<table class="table table-striped" filterable>
 	<thead>
 		<tr>
 			<th>Name</th>
@@ -35,43 +42,43 @@
 </table>				
 
 <form method="post" action="create.php" class="form-horizontal" role="form">
-<fieldset>
-<legend>Add new user</legend>
-	<div class="form-group">
-		<label for="name" class="col-lg-2 control-label">Name:</label>
-		<div class="col-lg-10">
-			<input name="name" input="text" id="name" class="form-control" />
-		</div>
-	</div>
-
-	<div class="form-group">
-		<label for="email" class="col-lg-2 control-label">E-mail:</label>
-		<div class="col-lg-10">
-			<div class="input-group">
-				<span class="input-group-addon">@</span>
-				<input name="email" input="text" id="email" class="form-control" />
+	<fieldset>
+		<legend>Add new user</legend>
+		<div class="form-group">
+			<label for="name" class="col-lg-2 control-label">Name:</label>
+			<div class="col-lg-10">
+				<input name="name" input="text" id="name" class="form-control" />
 			</div>
 		</div>
-	</div>
 
-	<div class="form-group">
-		<label for="city" class="col-lg-2 control-label">City:</label>
-		<div class="col-lg-10">
-			<input name="city" input="text" id="city" class="form-control" />
+		<div class="form-group">
+			<label for="email" class="col-lg-2 control-label">E-mail:</label>
+			<div class="col-lg-10">
+				<div class="input-group">
+					<span class="input-group-addon">@</span>
+					<input name="email" input="text" id="email" class="form-control" />
+				</div>
+			</div>
 		</div>
-	</div>
 
-	<div class="form-group">
-		<label for="phone" class="col-lg-2 control-label">Phone:</label>
-		<div class="col-lg-10">
-			<input name="phone" input="text" id="phone" class="form-control" />
+		<div class="form-group">
+			<label for="city" class="col-lg-2 control-label">City:</label>
+			<div class="col-lg-10">
+				<input name="city" input="text" id="city" class="form-control" />
+			</div>
 		</div>
-	</div>
 
-	<div class="form-group">
-		<div class="col-lg-offset-2 col-lg-10">
-			<button type="submit" class="btn btn-primary">Add new user</button>
+		<div class="form-group">
+			<label for="phone" class="col-lg-2 control-label">Phone:</label>
+			<div class="col-lg-10">
+				<input name="phone" input="text" id="phone" class="form-control" />
+			</div>
 		</div>
-	</div>
+
+		<div class="form-group">
+			<div class="col-lg-offset-2 col-lg-10">
+				<button type="submit" class="btn btn-primary">Add new user</button>
+			</div>
+		</div>
 	</fieldset>
 </form>
